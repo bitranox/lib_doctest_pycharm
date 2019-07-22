262aa3178dfedafa06e8156566e35ae18f605915
bitranox <1uGToNSiYo85pWhQcqFu>
2019-07-23 00:24:49 +0200

    refractor

--- lib_doctest_pycharm/lib_doctest_pycharm.py
+++ lib_doctest_pycharm/lib_doctest_pycharm.py
@@ -5,12 +5,33 @@ logger = logging.getLogger()
 
 
 def setup_doctest_logger_for_pycharm(log_level: int = logging.INFO):
-    if is_pytest_running() or is_docrunner_running():
-        lib_log_utils.setup_console_logger_simple()
-        if not hasattr(logger, 'pycharm_doctest_logger_added'):
-            logger_add_streamhandler_to_sys_stdout()
-            logging.getLogger().setLevel(log_level)
-            logger.pycharm_doctest_logger_added = True
+    """
+    >>> logger=logging.getLogger()
+    >>> logger.setLevel(logging.INFO)
+    >>> setup_doctest_logger_for_pycharm()
+    >>> logger.info('test')     # now we have the output we want
+    test
+
+    """
+    # if is_pycharm_pytest_runner() or is_pycharm_docrunner():
+    # logging.getLogger().setLevel(log_level)
+    # logging.getLogger('root').setLevel(log_level)
+    # lib_log_utils.setup_console_logger_simple()
+    # if not hasattr(logger, 'pycharm_doctest_logger_added'):
+    # logger_add_streamhandler_to_sys_stdout()
+    # logging.getLogger().setLevel(log_level)
+    # logger.pycharm_doctest_logger_added = True
+
+    # pycharm doctest tested OK
+    if is_pycharm_docrunner():
+        logger_add_streamhandler_to_sys_stdout()
+        logger.pycharm_doctest_logger_added = True
+        logging.getLogger().setLevel(log_level)
+        logging.getLogger('root').setLevel(log_level)
+    elif is_pycharm_pytest_runner():
+        pass
+    elif is_pytest_py():
+        pass
 
 
 def logger_add_streamhandler_to_sys_stdout():
@@ -33,15 +54,22 @@ def is_doctest_stdout_handler_added() -> bool:
     return False
 
 
-def is_docrunner_running() -> bool:
+def is_pycharm_docrunner() -> bool:
     if 'docrunner.py' in sys.argv[0]:
         return True
     else:
         return False
 
 
-def is_pytest_running():
+def is_pycharm_pytest_runner():
     if 'pytest_runner.py' in sys.argv[0]:
+        return True
+    else:
+        return False
+
+
+def is_pytest_py():
+    if 'pytest.py' in sys.argv[0]:
         return True
     else:
         return False
