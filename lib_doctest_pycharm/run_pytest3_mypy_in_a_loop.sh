6f86d08f40a194e19217cc210bb353c26e8a61ab
bitranox <rnowotny1966@gmail.com>
2019-07-23 10:08:26 +0200

    refractor

--- lib_doctest_pycharm/run_pytest3_mypy_in_a_loop.sh
+++ lib_doctest_pycharm/run_pytest3_mypy_in_a_loop.sh
@@ -4,23 +4,27 @@ sudo_askpass="$(command -v ssh-askpass)"
 export SUDO_ASKPASS="${sudo_askpass}"
 export NO_AT_BRIDGE=1  # get rid of (ssh-askpass:25930): dbind-WARNING **: 18:46:12.019: Couldn't register with accessibility bus: Did not receive a reply.
 
-"$(command -v sudo 2>/dev/null)" git clone https://github.com/bitranox/lib_bash.git /usr/local/lib_bash 2>/dev/null
-"$(command -v sudo 2>/dev/null)" chmod -R 0755 /usr/local/lib_bash 2>/dev/null
-"$(command -v sudo 2>/dev/null)" chmod -R +x /usr/local/lib_bash/*.sh 2>/dev/null
-"$(command -v sudo 2>/dev/null)" /usr/local/lib_bash/install_or_update.sh 2>/dev/null
+if [[ ! -f /usr/local/lib_bash/install_or_update.sh ]]; then
+    "$(command -v sudo 2>/dev/null)" git clone https://github.com/bitranox/lib_bash.git /usr/local/lib_bash 2>/dev/null
+    "$(command -v sudo 2>/dev/null)" chmod -R 0755 /usr/local/lib_bash 2>/dev/null
+    "$(command -v sudo 2>/dev/null)" chmod -R +x /usr/local/lib_bash/*.sh 2>/dev/null
+    "$(command -v sudo 2>/dev/null)" /usr/local/lib_bash/install_or_update.sh 2>/dev/null
+else
+    /usr/local/lib_bash/install_or_update.sh
+fi
 
 source /usr/local/lib_bash/lib_helpers.sh
 
 
 function upgrade_pytest {
     clr_green "updating pytest"
-    pip3 install --upgrade pytest 2>/dev/null
-    pip3 install --upgrade pytest-pep8 2>/dev/null
+    pip3 install --upgrade "https://github.com/pytest-dev/pytest.git"
+    pip3 install --upgrade "https://github.com/davidraleigh/pytest-pep8.git"
 }
 
 function upgrade_mypy {
     clr_green "updating mypy"
-    pip3 install --upgrade pypy &>/dev/null
+    pip3 install --upgrade "https://github.com/python/mypy.git"
 }
 
 
@@ -40,5 +44,6 @@ function pytest_loop {
 upgrade_pytest
 upgrade_mypy
 pytest_loop
+
 
 
